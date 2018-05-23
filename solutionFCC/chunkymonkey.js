0d86f8665d6df26eead6f317cfd6096f76481aee
punya <punyachapagain@gmail.com>
2018-05-23 15:33:29 +1000

    truncet array and create multi demensional array

---
+++ solutionFCC/chunkymonkey.js
@@ -0,0 +1,27 @@
+
+
+function chunkArrayInGroups(arr, size) {
+
+    arrayLength = arr.length
+    var array = [];
+    var k = 0
+    for(var i = 0;i < arrayLength;i+=size){
+        
+        sliceArray = arr.slice(i,i + size)
+        array[k] = sliceArray
+        k+=1
+
+        
+    }
+    
+    console.log(array);
+    
+  
+  
+    // Break it up.
+    return array;
+  }
+  
+  chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 2)
+  
+  
--- solutionFCC/truncet.js
+++ solutionFCC/truncet.js
@@ -2,13 +2,37 @@
 
 function truncateString(str, num) {
   // Clear out that junk in your trunk
-  var stringLength = str.length;
+  var stringLength = str.length
   if(stringLength > num){
-    var str1 = str.slice(0,num);
-    return str1 + "...";
+      var stringLength = str.length;
+      let newNum;
+      for(let i = 0; i < stringLength; i++){
+          if(str[i] === " "){
+              if(i < num){
+              newNum = i
+                  
+              }
+              
+          }
+      }
+
+      console.log(newNum)
+      str1 = str.slice(0,newNum);
+      var letters = /^[A-Za-z]+$/;
+      if(str1[str1.length -1].match(letters)){
+          console.log(str1 + "...")
+          return str1 + "...";
+      }
+      else{
+          str1 = str.slice(0,-1);
+          return str1 + "..."
+      }
+      
+
   }
-  return str;
   
+  return str;
+
 }
 
 
