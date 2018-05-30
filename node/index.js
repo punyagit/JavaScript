796324ac38367eb5c9eb5c3d07050a56aeeff34f
punya <punyachapagain@gmail.com>
2018-05-30 12:23:08 +1000

    changing route in index

--- node/index.js
+++ node/index.js
@@ -3,26 +3,36 @@ const {students, teachers,fruits} = require('./app/index')// use colon to rename
 var express = require('express')
 var app = express()
 
+
+app.use((req,resp,next) => {
+    console.log(req.headers)
+    next()
+})
+
 //const fruits = require('./app/index')
 
-students.forEach(function(student){
-    console.log(student)
-})
+// students.forEach(function(student){
+//     console.log(student)
+// })
 
-teachers.forEach(teacher =>{
-    console.log(teacher)
-})
+// teachers.forEach(teacher =>{
+//     console.log(teacher)
+// })
 
-fruits.forEach(teacher =>{
-    console.log(teacher)
-})
+// fruits.forEach(teacher =>{
+//     console.log(teacher)
+// })
 
 // Express
 app.get('/', function (req, res) {
-    res.send('Hello World')
+    
+    res.send('Hello World')  // you can do resp.json(flie)
+    
   })
    
-  app.listen(4000)
+  app.listen(4000, (err) => {
+    console.log("Server Started")
+  })
 
 
 
