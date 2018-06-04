9031d38f8a7122f6d6666e326a357f8e8c317941
punya <punyachapagain@gmail.com>
2018-06-04 15:21:34 +1000

    mapping and recursion

--- getify/recursion.js
+++ getify/recursion.js
@@ -1,12 +1,12 @@
 function mult(...args) {
     //console.log(...args)
     //console.log(args.length)
-    if (args.length <= 2){
+    if (args.length <= 2){ // it will stop for ulimited loop
         //console.log(args.length)
         return args[0] * args[1]
     }
     //console.log("here is " + args[0])
-    return args[0] * mult(...args.slice(1))
+    return args[0] * mult(...args.slice(1)) // slice first number of args array
 }
 
 console.log(mult(3,4,5))
@@ -14,5 +14,9 @@ console.log(mult(3,4,5,7))
 console.log(mult(3,4,5,6,7,3))
 
 
+function doubleIt(v) {return v % 2}
 
+x = [1,2,3,4,5].map(doubleIt)
+
+console.log(x)
 
