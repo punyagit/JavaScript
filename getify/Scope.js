b104ee50882826bde4c177d6e5b05ae2aa31e434
punya <punyachapagain@gmail.com>
2018-06-06 08:44:31 +1000

    more on scope and closure

--- getify/Scope.js
+++ getify/Scope.js
@@ -11,5 +11,18 @@ function bar() {
 
 bar()
 console.log(foo) // bar : get the value from global scope
-console.log(bam) //  yay : crate the value globally because bam is undeclared variable
+console.log(bam); //  yay : crate the value globally because bam is undeclared variable
 //baz() // error because javascript can't make function in global scope
+
+var new1 = (function add(){
+    return {
+        pint: function(){
+                var foo = "hello";
+                console.log(foo);
+        }
+    }
+    //window.foo = pint()
+})()
+  
+
+new1.pint()
--- getify/closure.js
+++ getify/closure.js
@@ -13,3 +13,11 @@ function add2(fn1,fn2,fn3){
 }
 
 console.log(add2(foo(10), foo(42),foo(5)))
+
+
+
+
+///----
+for(var i = 1; i <=5; i++){
+    setTimeout(function(){console.log("i:" + i);
+                         },i*1000);}
