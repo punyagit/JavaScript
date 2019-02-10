6861f4a09708b17bbdfb3936e7849247835d0ad6
punya <punyachapagain@gmail.com>
2019-02-11 10:25:34 +1100

    high order function cahining fileter and map

---
+++ ES6-fcc/highOrderFunction.js
@@ -0,0 +1,14 @@
+// Find the square root of even number
+
+const realNumberArray = [4, 5.6, -9.8, 3.14, 42, 6, 8.34, -2];
+const squareList = (arr) => {
+    "use strict";
+    // change code below this line
+    const squaredIntegers = realNumberArray.filter((arr) => (arr > 0 && arr % 2 == 0)).map(
+        (arr) => Math.pow(arr, 2));
+    // change code above this line
+    return squaredIntegers;
+};
+// test your code
+const squaredIntegers = squareList(realNumberArray);
+console.log(squaredIntegers);
