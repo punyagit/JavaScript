dae18d3eb46a4867e52081c03279c96cc2a1daf6
punya <punyachapagain@gmail.com>
2019-02-11 10:28:26 +1100

    add default parameter to function

---
+++ ES6-fcc/defaultParameter.js
@@ -0,0 +1,11 @@
+
+// Default parameter let you add the default value if value is not passed
+
+const increment = (function () {
+    "use strict";
+    return function increment(number, value = 1) {
+        return number + value;
+    };
+})();
+console.log(increment(5, 2)); // returns 7
+console.log(increment(5)); // returns 6
