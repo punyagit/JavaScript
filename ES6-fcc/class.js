e30d72b996a44e78e2474650897652134dfae4b3
punya <punyachapagain@gmail.com>
2019-02-11 12:23:20 +1100

    add getter and setter method in class

--- ES6-fcc/class.js
+++ ES6-fcc/class.js
@@ -13,3 +13,31 @@ const Vegetable = makeClass();
 const carrot = new Vegetable('carrot');
 console.log(carrot.name); // => should be 'carrot'
 
+
+/// getter and setter in a class
+
+function makeClass() {
+    "use strict";
+    /* Alter code below this line */
+    class Thermostat {
+        construcor(temperature) {
+            this.temperature = temperature;
+        }
+        get temp() {
+
+        }
+
+        set temp(celcius) {
+
+        }
+    }
+
+    /* Alter code above this line */
+    return Thermostat;
+}
+const Thermostat = makeClass();
+const thermos = new Thermostat(76); // setting in Fahrenheit scale
+let temp = thermos.temperature; // 24.44 in C
+thermos.temperature = 26;
+temp = thermos.temperature; // 26 in C
+
