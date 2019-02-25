8901ab7035a52106e0394020eb01825a2e3ec81d
punya <punyachapagain@gmail.com>
2019-02-26 09:35:53 +1100

    JS object

---
+++ basic-dataStructure-fcc/javaScriptObject.js
@@ -0,0 +1,18 @@
+let foods = {
+    apples: 25,
+    oranges: 32,
+    plums: 28
+};
+
+// change code below this line
+foods["bananas"] = 13;
+foods["grapes"] = 13;
+foods["strawberries"] = 13;
+
+
+
+
+
+// change code above this line
+
+console.log(foods);
