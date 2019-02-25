fdde0cdccc0a0d7f3f76daf8f338aa240994806a
punya <punyachapagain@gmail.com>
2019-02-26 09:50:31 +1100

    check if object have a key (multiple key)

--- basic-dataStructure-fcc/javaScriptObject.js
+++ basic-dataStructure-fcc/javaScriptObject.js
@@ -16,3 +16,30 @@ foods["strawberries"] = 13;
 // change code above this line
 
 console.log(foods);
+
+let users = {
+    Alan: {
+        age: 27,
+        online: true
+    },
+    Jeff: {
+        age: 32,
+        online: true
+    },
+    Sarah: {
+        age: 48,
+        online: true
+    },
+    Ryan: {
+        age: 19,
+        online: true
+    }
+};
+
+function isEveryoneHere(obj) {
+    // change code below this line
+    return (users.hasOwnProperty('Alan', 'Jeff', 'Sarah', 'Ryan')) ? true : false;
+    // change code above this line
+}
+
+console.log(isEveryoneHere(users));
