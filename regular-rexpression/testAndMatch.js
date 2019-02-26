d1bdcb460296d021011ee1752b6a180c98a89d5f
punya <punyachapagain@gmail.com>
2019-02-27 08:41:19 +1100

    ignoring case regex

---
+++ regular-rexpression/testAndMatch.js
@@ -0,0 +1,12 @@
+
+// Using test to match the string
+let myString = "Hello, World!";
+let myRegex = /Hello/;
+let result = myRegex.test(myString); // return true
+
+
+// Using OR operator to match multiple string
+let myRegex1 = /dog|cat|bird|fish/;
+
+// To Ignore case Just use i 
+myRegex = /dog|cat|bird|fish/i
