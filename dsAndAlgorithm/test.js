39248caa4aabf1181347180ab24dc193d2810d01
punya <punyachapagain@gmail.com>
2019-02-26 12:59:53 +1100

    solve using filter

--- dsAndAlgorithm/test.js
+++ dsAndAlgorithm/test.js
@@ -1,72 +1,23 @@
-var denom = [
-    { name: 'ONE HUNDRED', val: 100.00 },
-    { name: 'TWENTY', val: 20.00 },
-    { name: 'TEN', val: 10.00 },
-    { name: 'FIVE', val: 5.00 },
-    { name: 'ONE', val: 1.00 },
-    { name: 'QUARTER', val: 0.25 },
-    { name: 'DIME', val: 0.10 },
-    { name: 'NICKEL', val: 0.05 },
-    { name: 'PENNY', val: 0.01 }
-];
+function diffArray(arr1, arr2) {
 
-function checkCashRegister(price, cash, cid) {
-    var output = { status: null, change: [] };
-    var change = cash - price;
-
-    // Transform CID array into drawer object
-    var register = cid.reduce(function (acc, curr) {
-        acc.total += curr[1];
-        acc[curr[0]] = curr[1];
-        return acc;
-    }, { total: 0 });
-
-    // Handle exact change
-    if (register.total === change) {
-        output.status = 'CLOSED';
-        output.change = cid;
-        return output;
-    }
-
-    // Handle obvious insufficient funds
-    if (register.total < change) {
-        output.status = 'INSUFFICIENT_FUNDS';
-        return output;
-    }
-
-    // Loop through the denomination array
-    var change_arr = denom.reduce(function (acc, curr) {
-        var value = 0;
-        // While there is still money of this type in the drawer
-        // And while the denomination is larger than the change remaining
-        while (register[curr.name] > 0 && change >= curr.val) {
-            change -= curr.val;
-            register[curr.name] -= curr.val;
-            value += curr.val;
-
-            // Round change to the nearest hundreth deals with precision errors
-            change = Math.round(change * 100) / 100;
+    var newArr = [];
+    var length = Math.max(arr1.length, arr2.length)
+    for (let i = 0; i < length; i++) {
+        if (i < arr1.length) {
+            if (!arr2.includes(arr1[i])) {
+                newArr.push(arr1[i])
+            }
         }
-        // Add this denomination to the output only if any was used.
-        if (value > 0) {
-            acc.push(curr.name, value);
+        if (i < arr2.length) {
+            if (!arr1.includes(arr2[i])) {
+                newArr.push(arr2[i])
+            }
         }
-        return acc; // Return the current change_arr
-    }, []); // Initial value of empty array for reduce
-
-    // If there are no elements in change_arr or we have leftover change, return
-    // the string "Insufficient Funds"
-    if (change_arr.length < 1 || change > 0) {
-        output.status = 'INSUFFICIENT_FUNDS';
-        return output;
     }
 
-    // Here is your change, ma'am.
-    output.status = 'OPEN';
-    output.change = change_arr;
-    console.log(output)
-    return output;
+    // Same, same; but different.
+    console.log(newArr)
+    return newArr;
 }
 
-// test here
-checkCashRegister(19.50, 20.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]]);
+diffArray([1, "calf", 3, "piglet"], [1, "calf", 3, 4]);
---
+++ intermediateAlgorithm/diffTwoArray.js
@@ -0,0 +1,33 @@
+/// using filter and conventional method
+
+function diffArray(arr1, arr2) {
+
+    var array3 = arr1
+        .filter(el => !arr2.includes(el))
+        .concat(
+            arr2.filter(el => !arr1.includes(el))
+        )
+
+
+    // var newArr = [];
+    // var length = Math.max(arr1.length, arr2.length)
+    // for (let i = 0; i < length; i++) {
+    //     if (i < arr1.length) {
+    //         if (!arr2.includes(arr1[i])) {
+    //             newArr.push(arr1[i])
+    //         }
+    //     }
+    //     if (i < arr2.length) {
+    //         if (!arr1.includes(arr2[i])) {
+    //             newArr.push(arr2[i])
+    //         }
+    //     }
+    // }
+
+    // // Same, same; but different.
+    // console.log(newArr)
+    // return newArr;
+    console.log(array3);
+}
+
+diffArray([1, "calf", 3, "piglet"], [1, "calf", 3, 4]);
