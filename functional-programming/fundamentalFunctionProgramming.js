33e1ec023eb3db29cafe440b0ead0ad1acfb717a
punya <punyachapagain@gmail.com>
2019-02-27 14:04:20 +1100

    adding fundamentals

--- functional-programming/fundamentalFunctionProgramming.js
+++ functional-programming/fundamentalFunctionProgramming.js
@@ -10,3 +10,21 @@
 //  into the function as an argument.
 
 // Slice method in array is a functional approach because it return new array
+
+//Functional Programming: Combine Two Arrays Using the concat Method
+[1, 2, 3].concat([4, 5, 6]);
+
+//Functional Programming: Add Elements to the End of an Array Using concat Instead of push
+
+// Functional programming use map, filter, reduce instead of for loop, foreach loop.
+
+//Functional Programming: Sort an Array Alphabetically using the sort Method
+
+//Functional Programming: Combine an Array into a String Using the join Method
+
+//Functional Programming: Use the every Method to Check that Every Element in an Array Meets a Criteria
+
+//Functional Programming: Use the some Method to Check that Any Elements in an Array Meet a Criteria
+
+
+//Functional Programming: Introduction to Currying and Partial Application
