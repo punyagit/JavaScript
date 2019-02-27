e7e8df86bec4cd90bf17d3bd617e7ca0f003148f
punya <punyachapagain@gmail.com>
2019-02-27 13:00:18 +1100

    functional approach fundamental

---
+++ functional-programming/fundamentalFunctionProgramming.js
@@ -0,0 +1,12 @@
+// Recall that in functional programming, changing or altering things is called mutation, and the outcome
+// is called a side effect. A function, ideally, should be a pure function, meaning that it does not cause 
+// any side effects.
+
+
+//////////////////////
+
+// Another principle of functional programming is to always declare your dependencies explicitly. This means
+//  if a function depends on a variable or object being present, then pass that variable or object directly 
+//  into the function as an argument.
+
+// Slice method in array is a functional approach because it return new array
