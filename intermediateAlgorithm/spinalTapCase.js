826c1660930ab1cc2aaa0701857a542af25e09eb
punya <punyachapagain@gmail.com>
2019-03-02 17:29:48 +1100

    final with using relace and regex

--- intermediateAlgorithm/spinalTapCase.js
+++ intermediateAlgorithm/spinalTapCase.js
@@ -1,7 +1,6 @@
 function spinalCase(str) {
     string1 = ""
-    //let newStr = str.replace(/[_\s]/g, "-")
-    //console.log(newStr)
+
     for (let i = 0; i < str.length; i++) {
         if (/[^_-\s]/.test(str[i - 1]) && /[A-Z]/.test(str[i]) && i !== 0) {
             string1 += "-" + str[i]
@@ -12,7 +11,21 @@ function spinalCase(str) {
 
     }
     let newStr = (string1.toLowerCase()).replace(/[_\s]/g, "-")
-    console.log(string1)
+    console.log(newStr)
 }
 
 spinalCase("This Is Spinal Tap")
+
+// function spinalCase(str) {
+//     // Create a variable for the white space and underscores.
+//     var regex = /\s+|_+/g;
+
+//     // Replace low-upper case to low-space-uppercase
+//     str = str.replace(/([a-z])([A-Z])/g, '$1 $2');
+
+//     // Replace space and underscore with -
+//     return str.replace(regex, '-').toLowerCase();
+// }
+
+// // test here
+// spinalCase('This_Is SpinalTap');
