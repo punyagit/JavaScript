48dda31f681942084070f9ce8c18408c888c3b0e
punya <punyachapagain@gmail.com>
2019-03-02 23:26:50 +1100

    update file

--- intermediateAlgorithm/replace.js
+++ intermediateAlgorithm/replace.js
@@ -1,10 +1,9 @@
 function myReplace(str, before, after) {
-    var new1 = (before.charAt(0) === before.charAt(0).toUpperCase()) ?
+    return (before.charAt(0) === before.charAt(0).toUpperCase()) ?
         (str.replace(before, (after.charAt(0).toUpperCase() + after.slice(1))))
         : str.replace(before, after)
 
-    console.log(new1)
-    return new1;
+
 }
 
-myReplace("He is Sleeping on the couch", "Sleeping", "sitting")
+console.log(myReplace("He is Sleeping on the couch", "Sleeping", "sitting"))
