27c84387af10fbc1e3ff2a782109bb0ed5fb1922
punya <punyachapagain@gmail.com>
2019-03-07 21:47:39 +1100

    global object NaN and Infinity

--- mdn-docs/globalObject.js
+++ mdn-docs/globalObject.js
@@ -1,3 +1,10 @@
 Infinity // it is the global object
 
 NaN // it is also the property og global objects
+
+// to check NaN value 
+isNaN(Nan)// true
+isNaN(Number.NaN)// true
+
+// but 
+NaN === NaN // false  
