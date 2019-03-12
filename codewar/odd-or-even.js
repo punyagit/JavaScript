3032420a280865304e19428c9436a8be71a45974
punya <punyachapagain@gmail.com>
2019-03-12 22:32:23 +1100

    adding button to find odd or even

--- codewar/odd-or-even.js
+++ codewar/odd-or-even.js
@@ -1,10 +1,11 @@
 
 function even_or_odd(number) {
-    var num = (Boolean(number%2))
-    console.log(num ? "Odd" : "Even")
-    // ...
-  }
+  var num = (Boolean(number % 2))
+  console.log(num ? "Odd" : "Even")
+  alert(num)
+  // ...
+}
 
-  even_or_odd(2)
-  even_or_odd(21)
-  even_or_odd(0)
+even_or_odd(2)
+even_or_odd(21)
+even_or_odd(0)
