282bbf47d6fbc94abc702947528c29313729fa7b
punya <punyachapagain@gmail.com>
2019-03-13 20:35:52 +1100

    checking the value from input

--- codewar/odd-or-even.js
+++ codewar/odd-or-even.js
@@ -1,11 +1,13 @@
 
 let number = document.getElementById("number").value;
+console.log(number)
+
 function even_or_odd(number) {
-  console.log(number)
+  //console.log(number)
   var num = (Boolean(number % 2))
   console.log(num)
   alert(num)
-  // ...
+  // ...//////
 }
 
 // even_or_odd(2)
