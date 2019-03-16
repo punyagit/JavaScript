59433eeaae8196d6c4a45251f545e7e579caa377
punya <punyachapagain@gmail.com>
2019-03-16 20:51:37 +1100

    add fundamental of js with example

---
+++ interview-ques/fundamental.js
@@ -0,0 +1,15 @@
+//As we asserted in Chapter 1, JavaScript has typed values, not typed variables. 
+//The following built-in types are available:
+
+string
+number
+boolean
+null 
+ßundefined
+object 
+symbol//(new to ES6)
+
+// Except object all are primitve data types
+a = null
+typeof(a)// "object"  its a bug so take a special precaution when comparing null to object
+
