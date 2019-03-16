3cf062ef5b0e8a659d49b231f43b36ab1cc0f37e
punya <punyachapagain@gmail.com>
2019-03-16 19:40:09 +1100

    definiotion for closure

--- interview-ques/multipleArgument.js
+++ interview-ques/multipleArgument.js
@@ -11,9 +11,14 @@ const sum1 = (...args) => {
 
 
 
-
 sum(3, 4, 5)
+
+
+/// closure here inner function can acces the variable from outer function scope as well as in closure 
+// inner function can access the alue of outer function argument.
 sum1(4, 5)
+
+
 
     !function(x){
         (function(y){
