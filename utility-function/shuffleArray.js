e90240796fb33eba01c2f80d285af6ffd1e28053
punya <punyachapagain@gmail.com>
2019-03-18 11:18:42 +1100

    Fisher-yates shuffle implemented

--- utility-function/shuffleArray.js
+++ utility-function/shuffleArray.js
@@ -1,6 +1,20 @@
+// Fisher-yates shuffle algorithm    -- 0(n)
+
 function shuffleArray(arr){
-console.log(arr)
+    let newArray =[]
+    let c = 0
+    while(arr.length >= 1){
+        let randomNumber = Math.floor(Math.random() * arr.length);
+            newArray.push(arr[randomNumber])
+            arr[randomNumber] = arr[arr.length-1]
+            
+        
+        
+        arr.pop();
+    }
+    return newArray
+
 }
 
 
-shuffleArray([3,4,7,9])
+console.log(shuffleArray([3,"punya",7,"bisam",23,56,62]));
