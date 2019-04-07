f4fc2e22422dfbda1d33eca69ff792d2c99d5900
punya <punyachapagain@gmail.com>
2019-04-07 17:57:54 +1000

    implement binary search tree

--- dsAndAlgorithm/test.js
+++ dsAndAlgorithm/test.js
@@ -1,23 +1,5 @@
-function diffArray(arr1, arr2) {
-
-    var newArr = [];
-    var length = Math.max(arr1.length, arr2.length)
-    for (let i = 0; i < length; i++) {
-        if (i < arr1.length) {
-            if (!arr2.includes(arr1[i])) {
-                newArr.push(arr1[i])
-            }
-        }
-        if (i < arr2.length) {
-            if (!arr1.includes(arr2[i])) {
-                newArr.push(arr2[i])
-            }
-        }
-    }
-
-    // Same, same; but different.
-    console.log(newArr)
-    return newArr;
+function add(a, b) {
+    if (arguments.length === 1) return
+    return a + b
 }
-
-diffArray([1, "calf", 3, "piglet"], [1, "calf", 3, 4]);
+console.log(add(2, 3))
---
+++ utility-function/binarySearchTree.js
@@ -0,0 +1,14 @@
+
+
+function binarySearchTree(arr, val) {
+    let length = arr.length;
+    let value = (arr(Math.floor(length / 2)) === val) ? true : false;
+    (length < 2 && arr[0] === val) ? true : false;
+    if (value) {
+        return
+    }
+
+
+}
+
+binarySearchTree([1, 3, 5], 3)
