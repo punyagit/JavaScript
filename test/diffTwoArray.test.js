4756ecc9a3822b43af20e5d5af3eebdfa97323a7
punya <punyachapagain@gmail.com>
2019-04-11 20:46:01 +1000

    write test to check length of arguments

--- test/diffTwoArray.test.js
+++ test/diffTwoArray.test.js
@@ -10,4 +10,21 @@ test('should return an array with one item.', () => {
 });
 
 
+test('should return an array with ["pink wool"] ', () => {
+    expect(diffArray(["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"],
+        ["diorite", "andesite", "grass", "dirt", "dead shrub"])).toEqual(["pink wool"]);
+});
+
+test('should return an array with 3 items ', () => {
+    expect(diffArray([], ["snuffleupagus", "cookie monster", "elmo"]).length).toEqual(3);
+});
+console.log(diffArray.length)
+
+test('should have two arguments ', () => {
+    expect(diffArray.length).toEqual(2);
+});
+
+
+
+
 
