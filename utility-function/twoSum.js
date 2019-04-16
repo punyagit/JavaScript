88a4c5fea5611c55b98fe5b85fbd1b0afb5311c4
punya <punyachapagain@gmail.com>
2019-04-16 12:05:51 +1000

    solving twosum with better approach

--- utility-function/twoSum.js
+++ utility-function/twoSum.js
@@ -1,23 +1,48 @@
 
-// solve using hastable
-
+// Best 0logn worst 0n
 let twoSum = function (nums, target) {
+    let numObject = {}
+    let count = 0
+    for (var i = 0; i < nums.length; i++) {
+        let diff = target - nums[i];
+        count += 1
+        if (numObject.hasOwnProperty(diff) && numObject[diff] !== i) {
+            console.log(count)
+            return [numObject[diff], i];
+        }
+        numObject[nums[i]] = i
+    }
+    console.log(count)
+    return false
+
+
+}
+
+// solve using hastable and bettew than twoSum2 0nlogn
+
+
+let twoSum1 = function (nums, target) {
     let numObject = {};
+    let count = 0
     for (var i = 0; i < nums.length; i++) {
         let thisNum = nums[i];
         numObject[thisNum] = i;
+        count += 1
     }
+
     for (var i = 0; i < nums.length; i++) {
         let diff = target - nums[i];
+        count += 1
         if (numObject.hasOwnProperty(diff) && numObject[diff] !== i) {
+            console.log(count)
             return [i, numObject[diff]];
         }
     }
-
+    console.log(count)
     return false
 }
 
-
+// brute force way to solve 0n2
 let twoSum2 = function (nums, target) {
 
 
@@ -36,12 +61,13 @@ let twoSum2 = function (nums, target) {
     }
     return false
 };
-let array = Array.from(Array(600000).keys())
+let array = Array.from(Array(2000).keys())
+//let array = [2, 2, 7, 9, 5,]
 
 
 
 let start = new Date()
-twoSum(array, 300)
+console.log(twoSum(array, 5588))
 let timeTaken = new Date() - start
 
 console.log("time taken " + timeTaken + " Milli Second")
