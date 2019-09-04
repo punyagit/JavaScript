77bed75504687fd8b77e7ee5d1201e6436f97886
punya <punyachapagain@gmail.com>
2019-09-04 19:43:53 +1000

    add drawing eith mouse click

--- paint/script.js
+++ paint/script.js
@@ -1,5 +1,37 @@
 const canvas = document.getElementById('canvas');
+//const canvas = document.querySelector('canvas')
+canvas.addEventListener('mousedown', function (e) {
+    getCursorPosition(canvas, e)
+})
+
+
+
+let tempX = 0;
+
 const ctx = canvas.getContext('2d');
 
-ctx.fillStyle = 'green';
-ctx.fillRect(10, 10, 150, 100);
+function getCursorPosition(canvas, event) {
+    var rect = canvas.getBoundingClientRect();
+
+    var x = event.clientX - rect.left;
+    var y = event.clientY - rect.top;
+    console.log("x: " + x + " y: " + y);
+
+
+    if (tempX) {
+        ctx.strokeStyle = 'green';
+        ctx.lineWidth = 6;
+        ctx.lineTo(x, y);
+
+        ctx.stroke();
+    } else {
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+        console.log("fdf")
+        tempX = 1
+    }
+
+
+
+}
+
