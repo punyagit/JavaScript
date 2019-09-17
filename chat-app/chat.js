35beaad8b857122373ecbab388cde2f784cd187f
punya <punyachapagain@gmail.com>
2019-09-17 14:45:55 +1000

    add answer to chat

--- chat-app/chat.js
+++ chat-app/chat.js
@@ -1,1 +1,38 @@
-console.log("hello what is your name ?")
+// const readline = require('readline').createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// })
+// console.log("please type exit to close the chat")
+// let name = ""
+// while (name !== "exit") {
+//     readline.question(`Welcome to the chat ! Who is this `, (name) => {
+
+
+//         console.log(`Hi ${name}!  How can i help you`)
+//     })
+
+// }
+
+// readline.close()
+
+var readline = require('readline');
+var log = console.log;
+
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+var recursiveAsyncReadLine = function (question) {
+    rl.question(question, function (answer) {
+        if (answer == 'exit') //we need some base case, for recursion
+            return rl.close(); //closing RL and returning from function.
+        console.log("peter")
+        recursiveAsyncReadLine(""); //Calling this function again to ask new question
+    });
+};
+
+recursiveAsyncReadLine("How can i help you "); //we have to actually start our recursion somehow
+
+
+
