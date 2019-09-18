8255b9bb684f388fc11cd6465290f09039551d03
punya <punyachapagain@gmail.com>
2019-09-18 13:43:19 +1000

    add print file to print chatbot name

---
+++ chat-app/print.js
@@ -0,0 +1,35 @@
+const myName = () => {
+    console.log("========================================================================================")
+    setTimeout(
+        () =>
+            console.log("     ==========  JJJJJJJ    EEEEEEE     N       N      N       N     Y       Y  ========")
+        , 1000);
+    setTimeout(
+        () =>
+            console.log("     ==========      JJ     EE          N N     N      N N     N       Y   Y    ========")
+
+        , 2000);
+
+    setTimeout(
+        () =>
+            console.log("     ==========      JJ     EEEEEEE     N   N   N      N   N   N        Y Y     ========")
+        , 3000);
+    setTimeout(
+        () =>
+            console.log("     ==========  Jj  JJ     EE          N     N N      N     N N         YY     ========")
+        , 4000);
+    // setTimeout(
+    //     () =>
+    //         console.log("     ==========  JJ  JJ     EE          N      NN      N      NN         YY     ========")
+    //     , 5000);
+    setTimeout(
+        () =>
+            console.log("     ==========  JJJJJJ     EEEEEEE     N       N      N       N         YY     ========")
+        , 5000);
+    setTimeout(
+        () =>
+            console.log("     ===================================================================================")
+        , 6000);
+}
+
+myName()
