ad05665d4caa0b29ade4ca7358fc01293eb365f0
punya <punyachapagain@gmail.com>
2019-11-17 20:42:24 +1100

    add eslint airbnb style

--- codewar/countvowel.js
+++ codewar/countvowel.js
@@ -1,17 +1,18 @@
 function getCount1(str) {
-    var vowelsCount = 0;
-    var vowel = "aeiou"
-    // enter your majic here
-    for(var i = 0;i < str.length;i++){
-      if((vowel.indexOf(str[i])) >= 0){
-          vowelsCount+=1
-      }
-    } 
-    return vowelsCount;
+  let vowelsCount = 0;
+  const vowel = 'aeiou';
+  // enter your majic here
+  for (let i = 0; i < str.length; i++) {
+    if ((vowel.indexOf(str[i])) >= 0) {
+      vowelsCount += 1
+    }
+  }
+  return vowelsCount;
 }
 
 function getCount(str) {
-  return (str.match(/[aeiou]/ig)||[]).length;
+  return (str.match(/[aeiou]/ig) || []).length;
 }
 
-getCount("punya")
+getCount('punya');
+getCount1('name');
