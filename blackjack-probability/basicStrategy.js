f0525d6875d39c7011b1b12ed7546258393cfb1b
punya <punyachapagain@gmail.com>
2020-01-16 16:02:46 +1100

    refactor basic strategy to handle soft card

--- blackjack-probability/basicStrategy.js
+++ blackjack-probability/basicStrategy.js
@@ -4,29 +4,22 @@ function blackJackBasicStrategy(playerCard, dealerCard, card) {
   const dealerFaceUpValue = dealerCard[0];
   if (playerCard[0] === playerCard[1]) {
     if (playerCard[0] === 8 || playerCard[0] === 11) {
-      return splitCard(playerCard[0], card);
+      return [playerCard[0], playerCard[1]];
     }
 
     // prettier-ignore
 
     if ((playerCard[0] === 2 || playerCard[0] === 3 || playerCard[0] === 6 || playerCard[0] === 7 ||
     playerCard[0] === 9) && dealerFaceUpValue <= 6) {
-      return splitCard(playerCard[0], card);
+      return [playerCard[0], playerCard[1]];
     }
     return playerCardValue;
   }
   if (playerCard.includes(11)) {
-    return playerCardValue;
+    return [playerCard[0], playerCard[1]];
   }
 
   return playerCardValue;
-}
-
-function splitCard(cardToSplit, card) {
-  const value1 = cardToSplit;
-
-  const value2 = cardToSplit;
-  return [value1, value2];
 }
 
 module.exports = blackJackBasicStrategy;
