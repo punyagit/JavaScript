a435eda56a5f918633c91d5c56403558a3a543ca
punya <punyachapagain@gmail.com>
2020-01-16 15:28:52 +1100

    add function to check win or loose

--- blackjack-probability/basicStrategy.js
+++ blackjack-probability/basicStrategy.js
@@ -1,27 +1,28 @@
-function blackJackBasicStrategy(playerCard, playerNumber, dealerCard, card) {
+function blackJackBasicStrategy(playerCard, dealerCard, card) {
   const playerCardValue = playerCard[0] + playerCard[1];
 
   const dealerFaceUpValue = dealerCard[0];
   if (playerCard[0] === playerCard[1]) {
     if (playerCard[0] === 8 || playerCard[0] === 11) {
-      return splitCard(playerCard[0], playerNumber, card);
+      return splitCard(playerCard[0], card);
     }
 
     // prettier-ignore
 
     if ((playerCard[0] === 2 || playerCard[0] === 3 || playerCard[0] === 6 || playerCard[0] === 7 ||
     playerCard[0] === 9) && dealerFaceUpValue <= 6) {
-      return splitCard(playerCard[0], playerNumber, card);
+      return splitCard(playerCard[0], card);
     }
     return playerCardValue;
   }
   if (playerCard.includes(11)) {
     return playerCardValue;
   }
+
   return dealHardCard(playerCardValue, card);
 }
 
-function splitCard(cardToSplit, playerNumber, card) {
+function splitCard(cardToSplit, card) {
   const value1 = cardToSplit + card.shift();
 
   const value2 = cardToSplit + card.shift();
--- blackjack-probability/index.js
+++ blackjack-probability/index.js
@@ -3,47 +3,67 @@ const basicStrategy = require('./basicStrategy');
 
 function playBlackJack(numberOfPlayer, deck) {
   const card = shuflleCard(deck);
+  console.log(card.slice(4, 10));
   const dealCard = allDealCard(numberOfPlayer, card);
   console.log(dealCard);
   const dealerCard = dealCard[dealCard.length - 1];
   let dealerCardValue = dealerCard[0] + dealerCard[1];
+  let playerCardValueArray = [];
   for (let i = 0; i < numberOfPlayer; i += 1) {
     // prettier-ignore
-    let playerAfterStrategy = basicStrategy(dealCard[i], i + 1, dealerCard, card);
-    if (playerAfterStrategy !== []) {
+    let playerAfterStrategy = basicStrategy(dealCard[i], dealerCard, card);
+
+    if (!Array.isArray(playerAfterStrategy)) {
       while (playerAfterStrategy <= 16 && dealerCard[0] >= 7) {
         playerAfterStrategy += card.shift();
       }
-      while (dealerCardValue <= 16) {
-        dealerCardValue += card.shift();
-      }
+      playerCardValueArray.push(playerAfterStrategy);
+
       //console.log(`value of player ${playerAfterStrategy}`);
+    } else {
+      for (let i = 0; i < 2; i += 1) {
+        while (playerAfterStrategy[i] <= 16 && dealerCard[0] >= 7) {
+          playerAfterStrategy[i] = playerAfterStrategy[i] + card.shift();
+        }
+        playerCardValueArray.push(playerAfterStrategy[i]);
+      }
     }
-    if (playerAfterStrategy > 21) {
-      console.log(`player${i + 1} loose with ${playerAfterStrategy}`);
-    } else if (
-      playerAfterStrategy <= 21 &&
-      playerAfterStrategy === dealerCardValue
-    ) {
+  }
+
+  console.log(playerCardValueArray);
+  while (dealerCardValue <= 16) {
+    dealerCardValue += card.shift();
+  }
+
+  for (let i = 0; i < playerCardValueArray.length; i += 1) {
+    if (playerCardValueArray[i] > 21) {
       console.log(
-        `player${i +
-          1} tie with value ${playerAfterStrategy}  ${dealerCardValue}`,
-      );
-    } else if (playerAfterStrategy <= 21 && dealerCardValue >= 21) {
-      console.log(
-        `player${i + 1} win ${playerAfterStrategy}  ${dealerCardValue}`,
+        `Dealer win player${i + 1} busted with ${playerCardValueArray[i]}`,
       );
     } else if (
-      playerAfterStrategy <= 21 &&
-      playerAfterStrategy > dealerCardValue
+      playerCardValueArray[i] <= 21 &&
+      playerCardValueArray[i] === dealerCardValue
     ) {
       console.log(
-        `player${i + 1} win ${playerAfterStrategy}  ${dealerCardValue}`,
+        `player${i + 1} tie with value ${
+          playerCardValueArray[i]
+        }  ${dealerCardValue}`,
+      );
+    } else if (playerCardValueArray[i] <= 21 && dealerCardValue > 21) {
+      console.log(
+        `player${i + 1} win ${playerCardValueArray[i]}  ${dealerCardValue}`,
+      );
+    } else if (
+      playerCardValueArray[i] <= 21 &&
+      playerCardValueArray[i] > dealerCardValue
+    ) {
+      console.log(
+        `player${i + 1} win ${playerCardValueArray[i]}  ${dealerCardValue}`,
       );
     } else {
-      console.log(`dealer win ${dealerCardValue}`);
+      console.log(`dealer win ${playerCardValueArray[i]} ${dealerCardValue}`);
     }
   }
 }
 
-playBlackJack(3, 4);
+playBlackJack(1, 6);
