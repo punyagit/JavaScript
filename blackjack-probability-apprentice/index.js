9699f686c41e9cb4143fe67d542516b2c79eef7b
punya <punyachapagain@gmail.com>
2020-01-31 07:47:05 +1100

    add bust strategy to game

--- blackjack-probability-apprentice/index.js
+++ blackjack-probability-apprentice/index.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-lonely-if */
-const { playBlackJack, playBlackJackStar } = require('./playBlackJack');
+const { playBlackJack, playBlackJackStar, playBlackJackBust } = require('./playBlackJack');
 const { shuflleCard } = require('./dealCard');
 const { displayResult } = require('./printResult')
 
@@ -7,15 +7,16 @@ const { displayResult } = require('./printResult')
 function playGames(times) {
   const arr = [];
   const arr1 = [];
+  const arr2 = [];
   for (let k = 0; k < times; k += 1) {
     const deckOfCard = shuflleCard();
-    arr.push(playMultipleGames(deckOfCard));
+    arr.push(playMultipleBustGames(deckOfCard));
     arr1.push(playMultipleStarGames(deckOfCard));
+    arr2.push(playMultipleGames(deckOfCard));
   }
-  displayResult(arr, '4card')
-  console.log('------////-------------------------------------//=------')
+  displayResult(arr, 'bust')
   displayResult(arr1, '3card')
-
+  displayResult(arr2, '4card')
 }
 
 function playMultipleGames(deckOfCard) {
@@ -31,6 +32,12 @@ function playMultipleStarGames(deckOfCard) {
     return value[i];
   }
 }
+function playMultipleBustGames(deckOfCard) {
+  let value = playBlackJackBust(1, deckOfCard);
+  for (let i = 0; i < value.length; i += 1) {
+    return value[i];
+  }
+}
 
 
-playGames(100);
+playGames(200);
--- blackjack-probability-apprentice/playBlackJack.js
+++ blackjack-probability-apprentice/playBlackJack.js
@@ -6,7 +6,6 @@ const { printResult } = require('./printResult');
 function playBlackJack(numberOfPlayer, deckOfCard) {
 
   const card = [...deckOfCard];
-  //console.log(card.slice(0, 11))
   const dealCard = allDealCard(numberOfPlayer, card);
 
   const dealerCard = dealCard[dealCard.length - 1];
@@ -57,7 +56,7 @@ function playBlackJack(numberOfPlayer, deckOfCard) {
 function playBlackJackStar(numberOfPlayer, deckOfCard) {
 
   const card = [...deckOfCard];
-
+  //console.log(card.slice(0, 11))
   const dealStarCard = allDealCardStar(numberOfPlayer, card);
   const dealerCard = dealStarCard[dealStarCard.length - 1];
   let dealerCardValue = 0;
@@ -105,4 +104,58 @@ function playBlackJackStar(numberOfPlayer, deckOfCard) {
   dealerCardValue = handleDealerAceCard(dealerCard, card);
   return printResult(playerCardValueArray, dealerCardValue);
 }
-module.exports = { playBlackJack, playBlackJackStar }
+
+function playBlackJackBust(numberOfPlayer, deckOfCard) {
+
+  const card = [...deckOfCard];
+  //console.log(card.slice(0, 11))
+
+  const dealStarCard = allDealCardStar(numberOfPlayer, card);
+  const dealerCard = dealStarCard[dealStarCard.length - 1];
+  let dealerCardValue = 0;
+  const playerCardValueArray = [];
+
+  for (let i = 0; i < numberOfPlayer; i += 1) {
+    // prettier-ignore
+    let playerAfterStrategy = basicStrategy(dealStarCard[i], dealerCard);
+    if (!Array.isArray(playerAfterStrategy)) {
+      while (
+        playerAfterStrategy <= 11
+      ) {
+        let pullCard = card.shift();
+        if (pullCard === 11) {
+          if (pullCard + playerAfterStrategy > 21) {
+            playerAfterStrategy += 1;
+          } else {
+            playerAfterStrategy += pullCard;
+          }
+        } else {
+          playerAfterStrategy += pullCard;
+        }
+      }
+      playerCardValueArray.push(playerAfterStrategy);
+    } else {
+      // eslint-disable-next-line no-lonely-if
+      if (playerAfterStrategy[0] !== playerAfterStrategy[1]) {
+        playerCardValueArray.push(handleAceCard(playerAfterStrategy, card, dealerCard[0]));
+      } else {
+        for (let i = 0; i < 2; i += 1) {
+          while (
+            playerAfterStrategy[i] <= 11 ||
+            (playerAfterStrategy <= 16 && dealerCard[0] >= 7) || (playerAfterStrategy === 12 && dealerCard[0] <= 3)
+          ) {
+            playerAfterStrategy[i] += card.shift();
+          }
+          playerCardValueArray.push(playerAfterStrategy[i]);
+        }
+      }
+    }
+  }
+  //console.log(`dealerCard is ${dealerCard}`)
+  dealerCard[1] = card.shift();
+  dealerCardValue = handleDealerAceCard(dealerCard, card);
+  return printResult(playerCardValueArray, dealerCardValue);
+}
+
+
+module.exports = { playBlackJack, playBlackJackStar, playBlackJackBust }
