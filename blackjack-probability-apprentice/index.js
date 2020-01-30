e33a8de947097beee7a20641b5c8319231c22123
punya <punyachapagain@gmail.com>
2020-01-31 09:31:23 +1100

    refactor code

--- blackjack-probability-apprentice/index.js
+++ blackjack-probability-apprentice/index.js
@@ -10,14 +10,14 @@ function playGames(times) {
   const arr2 = [];
   for (let k = 0; k < times; k += 1) {
     const deckOfCard = shuflleCard();
-    let value = playMultipleGames(deckOfCard);
+    let value = playBlackJack(1, deckOfCard);
     if (value.length === 2) {
       arr.push(value[0]);
       arr.push(value[1]);
     } else {
       arr.push(value[0]);
     }
-    let value1 = playMultipleStarGames(deckOfCard);
+    let value1 = playBlackJackStar(1, deckOfCard);
     if (value1.length === 2) {
       arr1.push(value1[0]);
       arr1.push(value1[1]);
@@ -25,7 +25,7 @@ function playGames(times) {
       arr1.push(value1[0]);
     }
 
-    let value2 = playMultipleBustGames(deckOfCard);
+    let value2 = playBlackJackBust(1, deckOfCard);
     if (value2.length === 2) {
       arr2.push(value2[0]);
       arr2.push(value2[1]);
@@ -38,19 +38,4 @@ function playGames(times) {
   displayResult(arr2, '15 ---')
 }
 
-function playMultipleGames(deckOfCard) {
-  const value = playBlackJack(1, deckOfCard);
-  return value;
-}
-
-function playMultipleStarGames(deckOfCard) {
-  const value = playBlackJackStar(1, deckOfCard);
-  return value;
-}
-function playMultipleBustGames(deckOfCard) {
-  const value = playBlackJackBust(1, deckOfCard);
-  return value;
-}
-
-
-playGames(500);
+playGames(50);
